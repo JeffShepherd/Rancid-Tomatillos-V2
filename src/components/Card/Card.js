a02ac11439bcc1d83221a98c22e12823c513848a
Reggie Thompson <reggie.thompson4@gmail.com>
2021-03-27 14:49:09 -0600

    Add interactive styling using hover pseudo-selectors

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,9 +9,9 @@ const Card = ({ id, title, poster_path, release_date, average_rating, showMovieD
       key={id}
       id={id}
     >
-      <img className="movie-image" src={poster_path} alt="movie poster"></img>
-      <h2>{title}</h2>
-      <p>Freshness: {Math.round((average_rating / 10) * 100)}%</p>
+      <img className="movie-image" src={poster_path} alt="movie poster" />
+      <h2 className="card-text">{title}</h2>
+      <p className="card-text">Freshness: {Math.round((average_rating / 10) * 100)}%</p>
     </article>
   );
 };
