0b6210f63e981d0b2802b9a6f129bcafc6b54401
Reggie Thompson <reggie.thompson4@gmail.com>
2021-03-31 15:25:58 -0600

    WTest UI for server-related sad paths

--- cypress/integration/rancid-tomatillos-test.js
+++ cypress/integration/rancid-tomatillos-test.js
@@ -3,56 +3,74 @@ describe('Rancid Tomatillos', () => {
     cy.visit('http://localhost:3000/');
   });
 
-  it.skip('should display all movies on load', () => {
+  it('should display all movies on load', () => {
     cy.get('section').children().should('have.length', 40)
   });
 
-  it.skip('should reveal a selected movie\'s details when clicked', () => {
-    cy.get('article[id=337401]').click()
+  it('should reveal a selected movie\'s details when clicked', () => {
+    cy.get('a[id=337401]').click()
     cy.contains('When the Emperor')
   });
 
-  it.skip('should hide the application\'s home view when a movie\'s details are displayed', () => {
-    cy.get('article[id=337401]').click()
+  it('should hide the application\'s home view when a movie\'s details are displayed', () => {
+    cy.get('a[id=337401]').click()
     cy.get('section[class=movie-container]').should('not.exist')
   });
 
   it.skip('should hide the applications\'s details view when the user returns to the home page', () => {
-    cy.get('article[id=337401]').click()
+    cy.get('a[id=337401]').click()
     cy.get('button').click()
     cy.get('article[class=description]').should('not.exist');
   //**TEST FAILING DUE TO ASYNC NATURE, CONSIDER RE-WRITING W/ ROUTER**
+  });  
+})
+
+describe('App Sad Paths', () => {
+  it('should reveal an error message when the server returns a 404 status code', () => {
+    cy.intercept({
+      method: 'GET',
+      url: 'http://rancid-tomatillos.herokuapp.com/api/v2/movies'
+    },
+    {
+      statusCode: 404
+    })
+    cy.visit('http://localhost:3000/')
+    cy.get('h2[class=error-message]').should('contain', 'We\'re sorry, an error has occurred. Please try again later.')
   });
 
   it('should reveal an error message when the server returns a 500 status code', () => {
     cy.intercept({
       method: 'GET',
-      url: 'http://rancid-tomatillos.herokuapp.com/api/v2/movi'
+      url: 'http://rancid-tomatillos.herokuapp.com/api/v2/movies'
     },
     {
-      statusCode: 500,
-      body: { 
-        message: 'We\'re sorry, an error has occurred. Please try again later.'
-      }
+      statusCode: 500
     })
-    .wait(5000)
-    .get('h2[class=error-message]').should('contain', 'We\'re sorry, an error has occurred. Please try again later.')
+    cy.visit('http://localhost:3000/')
+    cy.get('h2[class=error-message]').should('contain', 'We\'re sorry, an error has occurred. Please try again later.')
+  });
+
+  it('should reveal an error message when the server returns a 404 status code', () => {
+    cy.intercept({
+      method: 'GET',
+      url: 'http://rancid-tomatillos.herokuapp.com/api/v2/movies/337401'
+    },
+    {
+      statusCode: 404
+    })
+    cy.visit('http://localhost:3000/337401')
+    cy.get('h2[class=error-message]').should('contain', 'Details for this movie are not available at this time. Please check back later.')
+  });
+
+  it('should reveal an error message when the server returns a 500 status code', () => {
+    cy.intercept({
+      method: 'GET',
+      url: 'http://rancid-tomatillos.herokuapp.com/api/v2/movies/337401'
+    },
+    {
+      statusCode: 500
+    })
+    cy.visit('http://localhost:3000/337401')
+    cy.get('h2[class=error-message]').should('contain', 'Details for this movie are not available at this time. Please check back later.')
   });
 })
-
-// describe('App Sad Paths', () => {
-//   // it('should reveal an error message when the server returns a 500 status code', () => {
-//   //   cy.intercept({
-//   //     method: 'GET',
-//   //     url: 'http://rancid-tomatillos.herokuapp.com/api/v2/movi'
-//   //   },
-//   //   {
-//   //     statusCode: 500,
-//   //     body: { 
-//   //       message: 'We\'re sorry, an error has occurred. Please try again later.'
-//   //     }
-//   //   })
-//   //   .wait(3000)
-//   //   .get('h2[class=error-message]').should('contain', 'We\'re sorry, an error has occurred. Please try again later.')
-//   // });
-// })
